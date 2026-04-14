7889416e9a41631d7376f4871f0d46f86e2c01c6
Dustin-Bellina <60012121+Dustin-Bellina@users.noreply.github.com>
2026-04-13 23:59:04 -0400

    Created defs.h and main.cpp
    Created definitions of a lot of useful things for the backend.
    
    Basically added:
    - Generate the main Town
    - Movement
    
    Full summary in main.cpp

---
+++ defs.h
@@ -0,0 +1,66 @@
+#define elif else if
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+#define KEY_0 48
+#define KEY_1 49
+#define KEY_2 50
+#define KEY_3 51
+#define KEY_4 52
+#define KEY_5 53
+#define KEY_6 54
+#define KEY_7 55
+#define KEY_8 56
+#define KEY_9 57
+
+#define KEY_a 97
+#define KEY_b 98
+#define KEY_c 99
+#define KEY_d 100
+#define KEY_e 101
+#define KEY_f 102
+#define KEY_g 103
+#define KEY_h 104
+#define KEY_i 105
+#define KEY_j 106
+#define KEY_k 107
+#define KEY_l 108
+#define KEY_m 109
+#define KEY_n 110
+#define KEY_o 111
+#define KEY_p 112
+#define KEY_q 113
+#define KEY_r 114
+#define KEY_s 115
+#define KEY_t 116
+#define KEY_u 117
+#define KEY_v 118
+#define KEY_w 119
+#define KEY_x 120
+#define KEY_y 121
+#define KEY_z 122
+
+#define SCREEN_HEIGHT 25
+#define SCREEN_WIDTH 80
+
+#define MAP_HEIGHT 20
+#define MAP_WIDTH 77
+#define MAX_ROOM_HEIGHT 6
+#define MAX_ROOM_WIDTH 25
+
+#define TL_WALL "╔"
+#define BL_WALL "╚"
+#define TR_WALL "╗"
+#define BR_WALL "╝"
+#define SIDE_WALL "║"
+#define TB_WALL "═"
+#define DOOR "╬"
+#define HALL "▒"
+#define FLOOR "."
+
+#define PLAYER "@"
+#define SHOP "⌂"
+#define SEWER_ENTERANCE "☼"
